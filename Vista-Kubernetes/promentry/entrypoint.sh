a176aa3e5b5fdd3439dd95b731fc94e1a837c2c8
RamSailopal <thelinuxguys@yahoo.com>
2021-10-25 15:07:03 +0100

    Adding / to the end

--- Vista-Kubernetes/promentry/entrypoint.sh
+++ Vista-Kubernetes/promentry/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cp /home/git/YottaDB-dashboard/Vista-Kubernetes/prometheus/* /opt/bitnami/prometheus/conf
+cp /home/git/YottaDB-dashboard/Vista-Kubernetes/prometheus/* /opt/bitnami/prometheus/conf/
 cd /opt/bitnami/prometheus
 /opt/bitnami/prometheus/bin/prometheus
