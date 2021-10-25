6ef94128d5184162778d6c4283c660dd27e93676
RamSailopal <thelinuxguys@yahoo.com>
2021-10-25 14:39:05 +0100

    Wrong path for Prometheus executable

--- Vista-Kubernetes/promentry/entrypoint.sh
+++ Vista-Kubernetes/promentry/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cp /home/git/YottaDB-dashboard/Vista-Kubernetes/prometheus/* /opt/bitnami/prometheus/conf
-/bin/prometheus
+cd /opt/bitnami/prometheus
+/opt/bitnami/prometheus/bin/prometheus
