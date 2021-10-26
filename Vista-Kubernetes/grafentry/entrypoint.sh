00a2142d16103e0e4095601ff2d7dab28eb22df6
RamSailopal <thelinuxguys@yahoo.com>
2021-10-26 12:56:47 +0100

    Now using an init container to change the ownership on /var/lib/grafana

--- Vista-Kubernetes/grafentry/entrypoint.sh
+++ Vista-Kubernetes/grafentry/entrypoint.sh
@@ -1,4 +1,10 @@
 cp -Rf /home/git/YottaDB-dashboard/Vista-Kubernetes/provisioning/* /etc/grafana/provisioning
-chown -R grafana /var/lib/grafana
-cd /usr/share/grafana
+export PATH="/usr/share/grafana/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+export GF_PATHS_CONFIG="/etc/grafana/grafana.ini"
+export GF_PATHS_DATA="/var/lib/grafana"
+export GF_PATHS_HOME="/usr/share/grafana"
+export GF_PATHS_LOGS="/var/log/grafana"
+export GF_PATHS_PLUGINS="/var/lib/grafana/plugins"
+export GF_PATHS_PROVISIONING="/etc/grafana/provisioning"
+cd /
 ./run.sh
