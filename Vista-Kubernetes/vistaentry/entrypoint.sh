c476781cd4bbdeb50488f1b0a7a57f3824406d57
RamSailopal <thelinuxguys@yahoo.com>
2021-10-28 11:51:27 +0100

    Change ownership recursively

--- Vista-Kubernetes/vistaentry/entrypoint.sh
+++ Vista-Kubernetes/vistaentry/entrypoint.sh
@@ -2,5 +2,5 @@
 mkdir /root/.ssh
 cp -Rf /home/git/YottaDB-dashboard/Vista-Kubernetes/.ssh/* /root/.ssh
 sed -i 's@#!/usr/bin/python@#!/usr/bin/python3@' /usr/local/YottaDB-dashboard/apiserver/apiserver.py
-chown nodevista:nodevista /home/nodevista/g
+chown -R nodevista:nodevista /home/nodevista/g
 /home/nodevista/bin/entryCombo.sh
